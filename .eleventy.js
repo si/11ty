253b36d4a171645edd87dc24be2a64ba59552f4f
Si Jobling <si@jobling.com>
2025-11-25 17:44:29 +0000

    Show a formatted publish date on post headers

--- .eleventy.js
+++ .eleventy.js
@@ -148,6 +148,12 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.addFilter("longPublishedDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLLL yyyy"
+    );
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
